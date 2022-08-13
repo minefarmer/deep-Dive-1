0e364c997bc68db6ec3695273f4f33b5c1575fc6
minefarmer <pgoldfarm@yahoo.com>
2022-08-13 17:28:50 -0500

    hi

---
+++ .history/class_notes/modules_packages_namespaces/module1_20220813162850.py
@@ -0,0 +1,14 @@
+# module1.py
+
+# print(f'Loading module1.py: __name__ = {__name__}')  # Loading module1.py: __name__ = __main__
+
+# if __name__ ==  '__main__':
+#     print('Module was executed...')  # Module was executed...
+
+
+
+# lesson 138
+print('executing module1...')
+
+value = 'module1 '
+
