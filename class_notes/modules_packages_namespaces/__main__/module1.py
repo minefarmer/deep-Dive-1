3095418b7afe8f760c7af0449c08613d5b178599
minefarmer <pgoldfarm@yahoo.com>
2022-08-14 22:06:09 -0500

    class_notes/modules_packages_namespaces/__main__: bye

---
+++ class_notes/modules_packages_namespaces/__main__/module1.py
@@ -0,0 +1,7 @@
+# module1.py
+
+print('loading module1.py: __name__ = {__main__}')  # loading module1.py: __name__ = {__main__}
+
+if __name__ == '__main__':
+    print('Module was executed...')
+
---
+++ class_notes/modules_packages_namespaces/__main__/run.py
@@ -0,0 +1,6 @@
+# run.py
+
+print(f'loading run.py: __name__ = {__name__}')  # loading run.py: __name__ = __main__
+
+
+
