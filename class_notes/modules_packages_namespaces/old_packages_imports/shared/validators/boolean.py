f3059792d8e69e45d3799b29e15808d1ec295f16
minefarmer <pgoldfarm@yahoo.com>
2022-08-25 20:01:21 -0500

    class_notes/modules_packages_namespaces/old_packages_imports: help

---
+++ class_notes/modules_packages_namespaces/old_packages_imports/shared/validators/boolean.py
@@ -0,0 +1,20 @@
+<<<<<<< HEAD:class_notes/modules_packages_namespaces/packages_imports/shared/validators/boolean.py
+# boolean
+=======
+# validators boolean.py
+>>>>>>> 28c91df64ca225ad16d59f5cc1a13abec337bf79:.history/class_notes/modules_packages_namespaces/packages/package_imports/common/validators/boolean_20220820122819.py
+
+
+def is_boolean(arg):
+    pass
+
+
+def boolean_helper_1():
+    pass
+
+
+def boolean_helper_2():
+<<<<<<< HEAD:class_notes/modules_packages_namespaces/packages_imports/shared/validators/boolean.py
+    pass
+=======
+>>>>>>> 28c91df64ca225ad16d59f5cc1a13abec337bf79:.history/class_notes/modules_packages_namespaces/packages/package_imports/common/validators/boolean_20220820122819.py
---
+++ class_notes/modules_packages_namespaces/old_packages_imports/shared/validators/date.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD:class_notes/modules_packages_namespaces/packages_imports/shared/validators/date.py
+# date.py
+=======
+# validators date
+>>>>>>> 28c91df64ca225ad16d59f5cc1a13abec337bf79:.history/class_notes/modules_packages_namespaces/packages/package_imports/common/validators/date_20220820121858.py
+
+def is_date(arg):
+    pass
+
+def date_helper_1():
+    pass
+
+
+
+def date_helper_2():
+    pass
