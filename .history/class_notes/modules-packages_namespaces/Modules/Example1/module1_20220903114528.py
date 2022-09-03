7694a80a77c88fd199cb7d56ac0093736f350b79
minefarmer <pgoldfarm@yahoo.com>
2022-09-03 11:49:12 -0500

    hi

--- .history/class_notes/modules-packages_namespaces/Modules/Example1/module1_20220903114528.py
+++ .history/class_notes/modules-packages_namespaces/Modules/Example1/module1_20220903114528.py
@@ -6,7 +6,7 @@ print('------------- Running {0} ------------'.format(__name__))
 def print_dict(header, d):
     print('\n\n-----------------------------------------')
     print('***** {0} *****'.format(header))
-    for key, value in d.item():
+    for key, value in d.items():
         print(key, value)
     print('---------------------------------------------\n\n')
 
